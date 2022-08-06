7fad3c85b3f21fddd14ca4aee7db4b557ec196ff
R1DF <33981824+R1DF@users.noreply.github.com>
2022-08-06 22:19:05 +0300

    tools moved to their own file

---
+++ src/tools.py
@@ -0,0 +1,24 @@
+# Imports
+import os
+from requests import get, ConnectionError
+
+# Functions
+def check_connection():
+    try:
+        get("http://api.scratch.mit.edu")
+        return True
+
+    except ConnectionError:
+        return False
+
+def await_enter(ld, to_exit=False):
+    input(ld["reusable"]["enterToContinue"] + "\n" if not to_exit else ld["reusable"]["enterToExit"] + "\n")
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def log(message):
+    print(message, end=" ")
+
+def break_line(amount=1):
+    print("\n" * amount)
