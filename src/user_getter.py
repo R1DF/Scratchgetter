55b5b4473cfc4521d264d22cd7f6c0c56d2f389c
R1DF <33981824+R1DF@users.noreply.github.com>
2022-08-07 14:28:38 +0300

    adapted to lang files

--- src/user_getter.py
+++ src/user_getter.py
@@ -7,6 +7,7 @@ import questionary
 # Constants
 API_USER = "https://api.scratch.mit.edu/users/~/"
 
+
 # UserProcess class
 class UserProcess:
     def __init__(self, master):
@@ -59,54 +60,54 @@ class UserProcess:
 
     def get_user(self, profile):
         # Connecting to the API
-        log("Connecting to the API...")
+        log(self.ld["reusable"]["connectingToAPIAwaiter"])
         response = requests.get(API_USER.replace("~", profile))
-        print("Done.")
+        print(self.ld["reusable"]["success"])
 
         # Loading data
-        log("Unpacking data...")
+        log(self.ld["reusable"]["collectingDataAwaiter"])
         json_data = response.json()
-        print("Done.")
+        insert_success(self.ld)
 
         # Creating dictionary, loading ST status and username
         profile_data = {"username": json_data["username"]}
-        log("Loading ST status...")
+        log(self.ld["get_user"]["loadingSTStatusAwaiter"])
         profile_data["isST"] = json_data["scratchteam"]
-        print("Done.")
+        insert_success(self.ld)
 
         # Getting join date
-        log("Loading join date...")
+        log(self.ld["get_user"]["loadingJoinDateAwaiter"])
         profile_data["join_date"] = self.format_date(json_data["history"]["joined"])
-        print("Done.")
+        insert_success(self.ld)
 
         # Getting user ID
-        log("Loading ID...")
+        log(self.ld["get_user"]["loadingIDAwaiter"])
         profile_data["userId"] = str(json_data["id"])
-        print("Done.")
+        insert_success(self.ld)
 
         # Delving into profile information, profile ID, loading status and bio
-        log("Loading profile contents...")
+        log(self.ld["get_user"]["loadingProfileContentsAwaiter"])
         json_data = json_data["profile"]
         profile_data["profileId"] = str(json_data["id"])
-        print("Done.")
+        insert_success(self.ld)
 
-        log("Loading \"About Me\" section...")
+        log(self.ld["get_user"]["loadingAboutMeAwaiter"])
         profile_data["aboutMe"] = json_data["bio"]
-        print("Done.")
+        insert_success(self.ld)
 
-        log("Loading \"What I'm Working On\" section...")
+        log(self.ld["get_user"]["loadingWIWOAwaiter"])
         profile_data["WIWO"] = json_data["status"]
-        print("Done.")
+        insert_success(self.ld)
 
         # Loading user's country
-        log("Loading country...")
+        log(self.ld["get_user"]["loadingCountryAwaiter"])
         profile_data["country"] = json_data["country"]
-        print("Done.")
+        insert_success(self.ld)
 
         # Loading images
-        log("Loading profile picture links...")
+        log(self.ld["get_user"]["loadingPFPLinksAwaiter"])
         profile_data["iconLinks"] = json_data["images"]
-        print("Done.")
+        insert_success(self.ld)
 
         # Returning all content
         return profile_data
@@ -115,70 +116,72 @@ class UserProcess:
         resolutions = ("90x90", "60x60", "55x55", "50x50", "32x32")
         with open(path + ".txt", "w") as f:
             f.write(details)
-            f.write("\n\nProfile pictures links:\n")
+            f.write(f"\n\n{self.ld['get_user']['PFPLinks']}\n")
             for resolution in range(len(resolutions)):
                 f.write(f"{resolutions[resolution]}: {profile_picture_links[resolutions[resolution]]}\n")
             f.write(f"\n{str(datetime.datetime.now())}")
 
     def reveal_user(self, details):
         # Getting output
-        output = open(os.getcwd() + "\\templates\\user.txt", "r").read()[29:]
+        output = open(os.getcwd() + "\\templates\\" + self.ld["linked"]["userTemplateFile"], "r").read()[
+                 self.ld["linked"]["templateOffset"]:]
 
         # Modifying template
         output = output.replace("USERNAME", details["username"])
         output = output.replace("IS_ST",
-                       "The user is a member of the Scratch Team." if details[
-                           "isST"] else "The user is not a member of the Scratch Team."
-                       )
+                                self.ld["get_user"]["isSTReplacer"] if details[
+                                    "isST"] else self.ld["get_user"]["notSTReplacer"]
+                                )
         output = output.replace("DATETIME", details["join_date"])
         output = output.replace("COUNTRY", details["country"])
         output = output.replace("PROFILE_ID", details["profileId"])
         output = output.replace("USER_ID", details["userId"])
 
         if details["aboutMe"].strip() == "":
-            output = output.replace("ABOUT_ME", "The user's \"About Me\" section is empty.")
+            output = output.replace("ABOUT_ME", self.ld["get_user"]["noAboutMeReplacer"])
         else:
-            output = output.replace("ABOUT_ME", f"About Me:\n{details['aboutMe']}")
+            output = output.replace("ABOUT_ME", f"{self.ld['get_user']['hasAboutMeReplacer']}\n{details['aboutMe']}")
 
         if details["WIWO"].strip() == "":
-            output = output.replace("WIWO", "The user's \"What I'm Working On\" section is empty.")
+            output = output.replace("WIWO", self.ld["get_user"]["noWIWOReplacer"])
         else:
-            output = output.replace("WIWO", f"What I'm Working On:\n{details['WIWO']}")
+            output = output.replace("WIWO", f"{self.ld['get_user']['hasWIWOReplacer']}\n{details['WIWO']}")
         print(output)
-        return output, details["iconLinks"]  # returns PFPs
+        return output, details["iconLinks"]  # returns PFPs too for later use
 
     def begin_user_process(self):
         # Getting user input
-        username = questionary.text(self.ld["get_user"]["enterUsername"], validate=lambda x: x.strip() != "").ask().strip()
+        username = questionary.text(self.ld["get_user"]["enterUsernameQuery"],
+                                    validate=lambda x: x.strip() != "").ask().strip()
 
         # Getting profile content
         clear()
-        print(self.ld["get_user"]["enterUsername"], username)
+        print(self.ld["get_user"]["user"], username)
         user_data = self.get_user(username)
         break_line()
         print(self.ld["get_user"]["userObtained"])
-        await_enter()
+        await_enter(self.ld)
 
         # Showing all content
         clear()
         formatted_response, profile_pictures = self.reveal_user(user_data)
 
         break_line()
-        print("Images for the profile pictures are accessible in a text file.")
+        print(self.ld["get_user"]["imageAvailability"])
         should_save = questionary.select(
-            "Would you like to save the text file?",
+            self.ld["get_user"]["saveFileQuery"],
             choices=[
-                "Yes",
-                "No"
+                self.ld["reusable"]["yes"],
+                self.ld["reusable"]["no"]
             ]
         ).ask()
 
-        if should_save == "Yes":
-            path = questionary.path("Enter file folder (Tab for hints):", only_directories=True,
+        if should_save == self.ld["reusable"]["yes"]:
+            path = questionary.path(self.ld["get_user"]["enterFileFolderQuery"], only_directories=True,
                                     default=os.getcwd() + "\\saves\\").ask()
-            file_name = questionary.text("Enter file name:", validate=lambda x: x.strip() != "",
+            file_name = questionary.text(self.ld["get_user"]["enterFileNameQuery"], validate=lambda x: x.strip() != "",
                                          default=username).ask().strip()
             self.save_user(path + "\\" + file_name, formatted_response, profile_pictures)
-            input("\nSuccess.\nHit Enter to exit the menu.\n")
+            print(self.ld["reusable"]["success"])
+            await_enter(self.ld)
         clear()
-
